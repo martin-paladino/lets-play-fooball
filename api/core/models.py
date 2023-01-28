1f3820bf9d886ac0d7dd8b523e22e2ebf59d8bf2
Martin <c.martin.pal@gmail.com>
2023-01-28 19:47:10 -0300

    formatting some files and secret_key as env var

--- api/core/models.py
+++ api/core/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
+
 class Team(models.Model):
     name = models.CharField(max_length=100, blank=False, null=False)
-    team_shield = models.ImageField(upload_to='media/team_shields/', blank=True, null=True)
+    team_shield = models.ImageField(
+        upload_to='media/team_shields/', blank=True, null=True)
     number_of_players = models.IntegerField(blank=True, null=True)
     average_age = models.FloatField(blank=True, null=True)
     country = models.CharField(max_length=50, blank=False, null=False)
@@ -19,15 +21,18 @@ class Player(models.Model):
     ]
 
     name = models.CharField(max_length=100)
-    picture = models.ImageField(upload_to='media/players/', blank=True, null=True)
-    team = models.ForeignKey(Team, on_delete=models.SET_NULL, blank=True, null=True, related_name='players')
+    picture = models.ImageField(
+        upload_to='media/players/', blank=True, null=True)
+    team = models.ForeignKey(
+        Team, on_delete=models.SET_NULL, blank=True, null=True, related_name='players')
     shirt_number = models.IntegerField(blank=False, null=False)
     position = models.CharField(max_length=50, blank=False, null=False)
     height = models.FloatField(blank=False, null=False)
     weight = models.FloatField(blank=False, null=False)
     birth_date = models.DateField(blank=False, null=False)
     age = models.IntegerField(blank=False, null=False)
-    foot = models.CharField(max_length=10, choices=FOOT_CHOICES, blank=False, null=False)
+    foot = models.CharField(
+        max_length=10, choices=FOOT_CHOICES, blank=False, null=False)
 
     def __str__(self):
         return self.name
