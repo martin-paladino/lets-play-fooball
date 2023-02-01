fb98a428198442c20338cc2ece2d4a74c1cb424b
Martin <c.martin.pal@gmail.com>
2023-02-01 00:43:45 -0300

    changes in serializers so teams show their players

--- api/core/serializers.py
+++ api/core/serializers.py
@@ -2,11 +2,19 @@ from . import models
 from rest_framework import serializers
 
 
+class PlayerSerialzerNested(serializers.ModelSerializer):
+    class Meta:
+        model = models.Player
+        fields = ('id', 'name')
+
+
 class TeamSerializer(serializers.ModelSerializer):
+    players = PlayerSerialzerNested(many=True, read_only=True)
+
     class Meta:
         model = models.Team
         fields = '__all__'
-        read_only_fields = ('average_age', 'number_of_players',)
+        read_only_fields = ('average_age', 'number_of_players', 'players')
 
 
 class PlayerSerializer(serializers.ModelSerializer):
@@ -14,6 +22,7 @@ class PlayerSerializer(serializers.ModelSerializer):
     team_id = serializers.IntegerField(write_only=True, required=False)
     team = TeamSerializer(read_only=True)
     picture = serializers.ImageField(required=False)
+    
     class Meta:
         model = models.Player
         fields = '__all__'
